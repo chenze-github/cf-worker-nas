f139bee00f1b6c8b5dbe5d9392c3d63e441aaa81
chenze-github <64511526+chenze-github@users.noreply.github.com>
2026-06-15 20:33:11 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    // You can view your logs in the Observability dashboard
+     const url = new URL(request.url);
+    console.log(123)
+    const backend =
+      `https://nas.chenwk.cn:5056${url.pathname}${url.search}`;
+
+    return fetch(backend, {
+      method: request.method,
+      headers: request.headers,
+      body: ["GET", "HEAD"].includes(request.method)
+        ? undefined
+        : request.body
+    });
+  }
+};
